15757c0e5b02a8410f05b4360bace9c0dc3ea8ba
Oviya <oviyasraj2001@gmail.com>
2024-05-22 22:52:05 -0700

    inital commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+
+
 const App = () => {
-  return <h1>Hello World!</h1>
+
+  const friends =[
+    {id:1,name:'John', age:20},
+    {id:2,name:'Karthi', age:24},
+  ]
+  return (
+    <div>
+      <h1>Friends</h1>
+     <ul>
+      {
+        friends.map(friend => 
+          <li key={friend.id}>{friend.name} {friend.age} </li>
+         
+        )
+      }
+     </ul>
+    </div>
+  )
 }
 
-export default App;
+export default App
