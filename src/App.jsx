1ef76637bdaff21690c2abf388b62f1c1ff91c93
Oviya <oviyasraj2001@gmail.com>
2024-05-26 04:04:15 -0700

    unfinished commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import './App.css';
+import Hello from './components/Hello';
+import  './App.css';
 
 const App = () => {
-
-
-
   return (
     <div>
-      <h1 className="Header">Price Card</h1>
-      <div className='container'>
-          
-        
-      </div>
+      <h1 className='header'>Price Card</h1>
+      <Hello />
     </div>
   )
 }
--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -1,11 +1,109 @@
-const Hello = (props) =>{
-
-    console.log(props);
-    return(
-        <div>
-           <p>Hello {props.name}!</p>
+import "./Hello.css";
+const Hello = () => {
+  return (
+    <div className="container">
+      <div class="row">
+        <div class="col-sm-4">
+          <div class="card">
+            <div class="card-body">
+            <p class="center">Free</p>
+              <h2 class="card-title font-weight-bold">$0/month</h2>
+              <hr />
+              <p class="card-text">
+              <ul class="list-unstyled my-4 text-small text-left">
+                <li class="mb-2">
+                  <i class="fa fa-check mr-2 text-small"></i>  Single user</li>
+                <li class="mb-2">
+                <i class="fa fa-check mr-2 text-small"></i>   50GB Storage</li>
+                <li class="mb-2">
+                <i class="fa fa-check mr-2 text-small"></i>   unlimited Public Projects</li>
+                <li class="mb-2">
+                <i class="fa fa-check mr-2 text-small"></i>   Community Access</li>
+                <li class="mb-2">
+                <i class="fa fa-check mr-2 text-small"></i>Unlimited Private Projects</li>
+                <li class="mb-2" id="times">
+                <i class="fa fa-times mr-2 text-small"></i>   Dedicated Phone Suport</li>
+                <li class="mb-2" id="times">
+                <i class="fa fa-times  mr-2 text-small" isExcluded></i>   Free Subdomain</li>
+                <li class="mb-2" id="times">
+                <i class="fa fa-times mr-2 text-small"></i>    Monthly Status Reports</li>
+                </ul>
+                
+              </p>
+              <a href="#" id="button" class="btn btn-primary">
+                Button
+              </a>
+            </div>
+          </div>
         </div>
-    )
-}
+        <div class="col-sm-4">
+          <div class="card">
+            <div class="card-body">
+            <p class="center">Plus</p>
+              <h2 class="card-title font-weight-bold">$9/month</h2>
+              <hr />
+              <p class="card-text">
+                <ul class="list-unstyled my-4 text-small text-left">
+                <li class="mb-2">
+                  <i class="fa fa-check mr-2 text-small"></i>  5 users</li>
+                <li class="mb-2">
+                <i class="fa fa-check mr-2 text-small"></i>   50GB Storage</li>
+                <li class="mb-2">
+                <i class="fa fa-check mr-2 text-small"></i>   unlimited Public Projects</li>
+                <li class="mb-2">
+                <i class="fa fa-check mr-2 text-small"></i>   Community Access</li>
+                <li class="mb-2">
+                <i class="fa fa-check mr-2 text-small"></i>Unlimited Private Projects</li>
+                <li class="mb-2">
+                <i class="fa fa-check mr-2 text-small"></i>   Dedicated Phone Suport</li>
+                <li class="mb-2">
+                <i class="fa fa-check mr-2 text-small"></i>   Free Subdomain</li>
+                <li class="mb-2" id="times">
+                <i class="fa fa-times mr-2 text-small"></i>    Monthly Status Reports</li>
+                </ul>
+              </p>
+              <a href="#" id="button" class="btn btn-primary" isExcluded>
+                Button
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="card">
+            <div class="card-body">
+              <p class="center">Pro</p>
+              <h2 class="card-title font-weight-bold">$49/month</h2>
+              <hr />
+              <p class="card-text">
+              <ul class="list-unstyled my-4 text-small text-left">
+                <li class="mb-2">
+                  <i class="fa fa-check mr-2 text-small"></i>  Unlimited users</li>
+                <li class="mb-2">
+                <i class="fa fa-check mr-2 text-small"></i>   50GB Storage</li>
+                <li class="mb-2">
+                <i class="fa fa-check mr-2 text-small"></i>   unlimited Public Projects</li>
+                <li class="mb-2">
+                <i class="fa fa-check mr-2 text-small"></i>   Community Access</li>
+                <li class="mb-2">
+                <i class="fa fa-check mr-2 text-small"></i>Unlimited Private Projects</li>
+                <li class="mb-2">
+                <i class="fa fa-check mr-2 text-small"></i>   Dedicated Phone Suport</li>
+                <li class="mb-2">
+                <i class="fa fa-check mr-2 text-small"></i>   Free Subdomain</li>
+                <li class="mb-2">
+                <i class="fa fa-check mr-2 text-small"></i>    Monthly Status Reports</li>
+                </ul>
+              </p>
+              <a href="#" id="btn-button" class="btn btn-primary">
+                Button
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  );
+};
 
 export default Hello;
